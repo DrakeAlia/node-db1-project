7dea73de7a9982dd1fae1f7074581c4b80f7dc2e
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-06-08 15:36:06 -0700

    add accounts and queries

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const accountsRouter = require('../accounts/accounts-router.js');
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
 
 module.exports = server;
